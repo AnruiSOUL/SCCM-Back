8f8b784da7dbf27374d5ae0c672f2324b96b40e1
Randall Crame <randallcrame@zipcoders-MacBook-Pro.local>
2017-03-23 15:51:31 -0400

    spring created for SCCM backend

---
+++ src/main/java/crame/randall/controller/HomeController.java
@@ -0,0 +1,19 @@
+package crame.randall.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by randallcrame on 3/23/17.
+ */
+
+@RestController
+public class HomeController
+{
+
+    @RequestMapping("/")
+    public String home()
+    {
+        return "SCCM ready to go";
+    }
+}
