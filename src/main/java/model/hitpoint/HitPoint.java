310e7e7cb8253fd1c04aa66bdc074e0aaf1a32eb
unknown <rcrame05@gmail.com>
2017-06-01 17:15:27 -0400

    hitpoint fields

--- src/main/java/model/hitpoint/HitPoint.java
+++ src/main/java/model/hitpoint/HitPoint.java
@@ -4,4 +4,7 @@ package model.hitpoint;
  * Created by AnruiSOUL on 6/1/2017.
  */
 public class HitPoint {
+    private Integer maxHP;
+    private Integer currentHP;
+    private Integer nonLethalHP;
 }
