af0a85730fe8ac1bac892347f35c4f7f36f4d91a
unknown <rcrame05@gmail.com>
2017-06-08 00:03:09 -0400

    add RequestMapping to CharacterScoresController

--- src/main/java/crame/randall/Controller/CharacterScoreController.java
+++ src/main/java/crame/randall/Controller/CharacterScoreController.java
@@ -1,19 +1,27 @@
 package crame.randall.Controller;
 
 import crame.randall.Entity.CharacterScore.CharacterScores;
-import crame.randall.Service.CharacterScoreService;
+import crame.randall.Service.CharacterScoresService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
 
 /**
  * Created by AnruiSOUL on 6/7/2017.
  */
+
+@RestController
 public class CharacterScoreController {
 
     @Autowired
-    private CharacterScoreService characterScoresService;
+    private CharacterScoresService characterScoresService;
 
-
+    @RequestMapping(value = "/characterscores", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createCharacterScores(@RequestBody CharacterScores characterScores){
         characterScoresService.createCharacterScores(characterScores);
     }
--- src/main/java/crame/randall/Service/CharacterScoresService.java
+++ src/main/java/crame/randall/Service/CharacterScoresService.java
@@ -2,11 +2,14 @@ package crame.randall.Service;
 
 import crame.randall.Entity.CharacterScore.CharacterScores;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by AnruiSOUL on 6/7/2017.
  */
-public class CharacterScoreService {
+
+@Service
+public class CharacterScoresService {
 
     @Autowired
     CharacterScores characterScores;
