5765fe45240d523b299e894797a95b8290887b57
unknown <rcrame05@gmail.com>
2017-06-18 14:06:23 -0400

    Character now has 4 fields

--- src/main/java/crame/randall/Entity/charactersheet/CharacterSheet.java
+++ src/main/java/crame/randall/Entity/charactersheet/CharacterSheet.java
@@ -1,19 +1,7 @@
 package crame.randall.Entity.charactersheet;
 
-import crame.randall.Entity.Weapon.Weapon;
-import crame.randall.Entity.CharacterScore.CharacterScores;
-import crame.randall.Entity.armor.Armor;
-import crame.randall.Entity.basicinformation.BasicInformation;
-import crame.randall.Entity.feats.Feat;
-import crame.randall.Entity.inventory.Item;
-import crame.randall.Entity.inventory.Money;
-import crame.randall.Entity.magicitems.MagicItem;
-import crame.randall.Entity.skills.Skill;
-import crame.randall.Entity.specialabilities.SpecialAbilities;
-import crame.randall.Entity.trait.Traits;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
 
 /**
  * Created by AnruiSOUL on 6/1/2017.
@@ -22,109 +10,41 @@ import java.util.ArrayList;
 @Configuration
 public class CharacterSheet {
 
-    private CharacterScores characterScores;
 
+  private String name;
+  private String race;
+  private String jobClass;
+  private int lvl;
 
-    private
-    private BasicInformation characterInformation;
-
-
-    private ArrayList<Armor> armory;
-    private ArrayList<Weapon> weaponArmory;
-    private ArrayList<Skill> skillList;
-    private ArrayList<Feat> featsList;
-    private ArrayList<SpecialAbilities> specialAbilitiesArrayList;
-    private ArrayList<Traits> traitsArrayList;
-    private ArrayList<MagicItem> magicItemsList;
-    private Money characterMoney;
-    private ArrayList<Item> itemList;
-
-    public BasicInformation getCharacterInformation() {
-        return characterInformation;
+    public String getName() {
+        return name;
     }
 
-    public void setCharacterInformation(BasicInformation characterInformation) {
-        this.characterInformation = characterInformation;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public CharacterScores getCharacterScores() {
-        return characterScores;
+    public String getRace() {
+        return race;
     }
 
-    public void setCharacterScores(CharacterScores characterScores) {
-        this.characterScores = characterScores;
+    public void setRace(String race) {
+        this.race = race;
     }
 
-
-    public ArrayList<Armor> getArmory() {
-        return armory;
+    public String getJobClass() {
+        return jobClass;
     }
 
-    public void setArmory(ArrayList<Armor> armory) {
-        this.armory = armory;
+    public void setJobClass(String jobClass) {
+        this.jobClass = jobClass;
     }
 
-    public ArrayList<Weapon> getWeaponArmory() {
-        return weaponArmory;
+    public int getLvl() {
+        return lvl;
     }
 
-    public void setWeaponArmory(ArrayList<Weapon> weaponArmory) {
-        this.weaponArmory = weaponArmory;
-    }
-
-    public ArrayList<Skill> getSkillList() {
-        return skillList;
-    }
-
-    public void setSkillList(ArrayList<Skill> skillList) {
-        this.skillList = skillList;
-    }
-
-    public ArrayList<Feat> getFeatsList() {
-        return featsList;
-    }
-
-    public void setFeatsList(ArrayList<Feat> featsList) {
-        this.featsList = featsList;
-    }
-
-    public ArrayList<SpecialAbilities> getSpecialAbilitiesArrayList() {
-        return specialAbilitiesArrayList;
-    }
-
-    public void setSpecialAbilitiesArrayList(ArrayList<SpecialAbilities> specialAbilitiesArrayList) {
-        this.specialAbilitiesArrayList = specialAbilitiesArrayList;
-    }
-
-    public ArrayList<Traits> getTraitsArrayList() {
-        return traitsArrayList;
-    }
-
-    public void setTraitsArrayList(ArrayList<Traits> traitsArrayList) {
-        this.traitsArrayList = traitsArrayList;
-    }
-
-    public ArrayList<MagicItem> getMagicItemsList() {
-        return magicItemsList;
-    }
-
-    public void setMagicItemsList(ArrayList<MagicItem> magicItemsList) {
-        this.magicItemsList = magicItemsList;
-    }
-
-    public Money getCharacterMoney() {
-        return characterMoney;
-    }
-
-    public void setCharacterMoney(Money characterMoney) {
-        this.characterMoney = characterMoney;
-    }
-
-    public ArrayList<Item> getItemList() {
-        return itemList;
-    }
-
-    public void setItemList(ArrayList<Item> itemList) {
-        this.itemList = itemList;
+    public void setLvl(int lvl) {
+        this.lvl = lvl;
     }
 }
