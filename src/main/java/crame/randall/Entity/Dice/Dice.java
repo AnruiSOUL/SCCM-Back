5df1cde8cfbba77fc14346eb625420192b423aae
AnruiSOUL <rcrame05@gmail.com>
2017-07-27 17:37:06 -0400

    static dice functions for rolling

--- src/main/java/crame/randall/Entity/Dice/Dice.java
+++ src/main/java/crame/randall/Entity/Dice/Dice.java
@@ -1,10 +1,21 @@
 package crame.randall.Entity.Dice;
 
-public class Dice {
+import java.util.Random;
 
+public class Dice {
     public Dice(){}
 
     static public int roll(int sides){
-        return 100;
+        Random random = new Random();
+        return random.nextInt(sides)+1;
     }
+
+    static public int[] multiDiceRoll(int numberOfDice, int diceSize){
+        int[] diceResults = new int[numberOfDice];
+        for (int i = 0; i < numberOfDice; i++) {
+            diceResults[i] = Dice.roll(diceSize);
+        }
+        return diceResults;
+    }
+
 }
--- src/test/java/crame/randall/DiceTest.java
+++ src/test/java/crame/randall/DiceTest.java
@@ -1,24 +1,39 @@
 package crame.randall;
 
 import crame.randall.Entity.Dice.Dice;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert;
 
 public class DiceTest {
     Dice dice;
-    int d6;
+    int d6, d20;
 
     @Before
     public void setUp() {
         dice = new Dice();
         d6 = Dice.roll(6);
+        d20 = Dice.roll(20);
     }
 
     @Test
-    public void  rollTest(){
-        boolean expected = true;
-        boolean actual = (d6 < 7 && d6 > 0 );
+    public void rollTest() {
+        boolean actual = (d6 < 7 && d6 > 0);
         Assert.assertTrue("Testing the call of dice will be less then 7", actual);
     }
+
+    @Test
+    public void multiRollTest() {
+        int expected = 4;
+        int actual = Dice.multiDiceRoll(4, 20).length;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public  void multiRollValueTest() {
+        int[] expected = {1,1,1,1};
+        int[] actual = Dice.multiDiceRoll(4,1);
+        Assert.assertArrayEquals(expected,actual);
+    }
 }
+
