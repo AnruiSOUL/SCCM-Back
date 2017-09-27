3ff6d7e80e9687eef51021336fab3db3e6b18b4d
AnruiSOUL <rcrame05@gmail.com>
2017-09-27 15:53:17 -0400

    creating Race entity

---
+++ src/main/java/crame/randall/Entity/race/Race.java
@@ -0,0 +1,18 @@
+package crame.randall.Entity.race;
+
+import crame.randall.Entity.CharacterScore.AbilityScore;
+import crame.randall.Entity.basicinformation.Size;
+import crame.randall.Entity.speed.Speed;
+import crame.randall.Entity.trait.Traits;
+
+import java.util.ArrayList;
+
+public class Race {
+    private Size size;
+    private Speed speed;
+    private ArrayList<Traits> racialTraits;
+    private ArrayList<AbilityScore> racialAbilityAdjustments;
+    private ArrayList<String> racialLanguages;
+
+
+}
